56a8b79f154e24d783e04f68358e27481ef1a7fb
GeraldKhosa <gerald.khosa@gmail.com>
2023-07-24 03:59:25 -0700

    add string.c file

---
+++ string.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * string - print the string.
+ * @args: the variadic arguments.
+ *
+ * Return: number of printed characters.
+ */
+int string(va_list args)
+{
+	char *str;
+	int x;
+	int counted;
+
+	str = va_arg(args, char *);
+	x = 0;
+	counted = 0;
+	while (str[x] != '\0')
+	{
+		write(1, &str[x], 1);
+		x++;
+		counted++;
+	}
+	return (counted);
+}
